7742ea604c06130dcdde6004665a678ccfe314bb
Marc Riera Casals <mrc2407@gmail.com>
2017-03-23 11:10:56 +0100

    Add census authorization handler

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # frozen_string_literal: true
 Decidim.configure do |config|
-  config.application_name = "My Application Name"
+  config.application_name = "Decidim Terrassa"
   config.mailer_sender    = "change-me@domain.org"
-  config.authorization_handlers = [ExampleAuthorizationHandler]
+  config.authorization_handlers = [CensusAuthorizationHandler]
 
   # Uncomment this lines to set your preferred locales
   config.available_locales = %i{ca es}
