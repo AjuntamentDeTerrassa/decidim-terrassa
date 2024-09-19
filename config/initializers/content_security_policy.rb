07ce8e7e568ee07926b618d670d872bc7e985fb6
Fernando Blat <fernando@blat.es>
2024-09-19 05:29:01 +0200

    Fix CSP

---
+++ config/initializers/content_security_policy.rb
@@ -0,0 +1,10 @@
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https, :unsafe_inline
+  policy.font_src :self, :https, :data
+  policy.img_src :self, :https, :data, "*.terrassa.cat", "terrassa.cat", "*.hereapi.com"
+  policy.object_src :none
+  policy.script_src :self, :https, :unsafe_inline, :unsafe_eval, "terrassa.cat"
+  policy.style_src :self, :https, :unsafe_inline
+  policy.frame_src :self, :https, "*.youtube.com", "*.vimeo.com", "www.youtube-nocookie.com"
+  policy.connect_src :self, :https, "terrassa.cat", "*.hereapi.com", "*.jsdelivr.net", "*.amazonaws.com"
+end
