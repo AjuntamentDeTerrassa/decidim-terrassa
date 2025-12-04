0f8e32c6fb7d61333a625c9d41ff7c8e41680d59
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-12-04 21:47:35 +0100

    Configure extra content security policies

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -403,17 +403,16 @@ Decidim.configure do |config|
     "#{class_identifier}-#{resource.id}"
   end
 
-  # This only works in 0.28
-  # config.content_security_policies_extra = {
-  #   "default-src" => %w('self' 'unsafe-inline'),
-  #   "script-src" => %w('self' 'unsafe-inline' 'unsafe-eval' *.terrassa.cat),
-  #   "style-src" => %w('self' 'unsafe-inline'),
-  #   "img-src" => %w('self' *.hereapi.com data: *.amazonaws.com terrassa.cat *.terrassa.cat),
-  #   "font-src" => %w('self'),
-  #   "connect-src" => %w('self' *.hereapi.com *.jsdelivr.net *.amazonaws.com),
-  #   "frame-src" => %w('self' *.youtube.com www.youtube-nocookie.com player.vimeo.com *.google.com *.airtable.com),
-  #   "media-src" => %w('self')
-  # }
+  config.content_security_policies_extra = {
+    "default-src" => %w(self unsafe-inline),
+    "script-src" => %w(self unsafe-inline unsafe-eval *.terrassa.cat),
+    "style-src" => %w(self unsafe-inline fonts.googleapis.com maxcdn.bootstrapcdn.com),
+    "img-src" => %w(self *.hereapi.com data: *.amazonaws.com terrassa.cat *.terrassa.cat),
+    "font-src" => %w(self fonts.gstatic.com maxcdn.bootstrapcdn.com),
+    "connect-src" => %w(self *.hereapi.com *.jsdelivr.net *.amazonaws.com fonts.googleapis.com maxcdn.bootstrapcdn.com),
+    "frame-src" => %w(self *.youtube.com www.youtube-nocookie.com player.vimeo.com *.google.com *.airtable.com),
+    "media-src" => %w(self)
+  }
 end
 
 Decidim::Verifications.register_workflow(:census_authorization_handler) do |auth|
