a6053f1f603d6f7b8f5f7556fe8781361a40da57
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-12-04 16:07:43 +0100

    Add letter_opener path

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => '/sidekiq'
   end
 
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
