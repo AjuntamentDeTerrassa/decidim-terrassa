798e3f0e265d812f8823c1d7b6c76ad0046a8980
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-12-05 14:36:26 +0100

    Do not override S3 in development environment

--- config/initializers/aws_sdk_service.rb
+++ config/initializers/aws_sdk_service.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-require "aws-sdk-s3"
-require "active_storage/client_service_initialization_overrides"
+unless Rails.env.development?
+  require "aws-sdk-s3"
+  require "active_storage/client_service_initialization_overrides"
 
-Aws::S3::Resource.class_eval do
-  prepend ActiveStorage::ClientServiceInitializationOverrides
+  Aws::S3::Resource.class_eval do
+    prepend ActiveStorage::ClientServiceInitializationOverrides
+  end
 end
